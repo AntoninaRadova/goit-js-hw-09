9b40b229d792b20f483107150bee8b2919223181
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-03-22 12:47:20 +0200

    Add files via upload

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,27 +1,27 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
-
-const onBtnChangeColor = document.querySelector("[data-start]");
-const onBtnStopColor = document.querySelector("[data-stop]");
-let idInterval = null;
-    
-onBtnChangeColor.addEventListener("click", () => {
-  idInterval = setInterval(() => {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+  
+  const onBtnChangeColor = document.querySelector("[data-start]");
+  const onBtnStopColor = document.querySelector("[data-stop]");
+  let idInterval = null;
       
-    const colorRandom = getRandomHexColor();
-    document.body.style.backgroundColor = colorRandom;
-
-  }, 1000)
-    
-  onBtnChangeColor.disabled = true;
-  onBtnStopColor.disabled = false;
-    
-});
-
-onBtnStopColor.addEventListener('click', () => {
-    clearInterval(idInterval);
-    onBtnStopColor.disabled = true;
-    onBtnChangeColor.disabled = false;
-
-});
+  onBtnChangeColor.addEventListener("click", () => {
+    idInterval = setInterval(() => {
+        
+      const colorRandom = getRandomHexColor();
+      document.body.style.backgroundColor = colorRandom;
+  
+    }, 1000)
+      
+    onBtnChangeColor.disabled = true;
+    onBtnStopColor.disabled = false;
+      
+  });
+  
+  onBtnStopColor.addEventListener('click', () => {
+      clearInterval(idInterval);
+      onBtnStopColor.disabled = true;
+      onBtnChangeColor.disabled = false;
+  
+  });
