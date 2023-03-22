8fc0f0982b555dc93a5ff825b6d9c3666138376a
AntoninaRadova <110526254+AntoninaRadova@users.noreply.github.com>
2023-03-22 12:17:09 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+
+  });
 }
+
+form.addEventListener('submit', evt => {
+  evt.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = evt.currentTarget;
+
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  for (let position = 1; position <= amountValue; position += 1) {
+    const delay = delayValue + stepValue * (position - 1);
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
+
+  }
+});
